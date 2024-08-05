62bf394ecc02d1302a4f9069862fbfb20b094364
heliwei <heliwei@tal.com>
2024-08-05 14:51:58 +0800

    bitmap impl

---
+++ bitmap.go
@@ -0,0 +1,25 @@
+package file_tranport
+
+type bitmap struct {
+	data []byte
+	size int
+}
+
+func NewBitmap(size int) *bitmap {
+	return &bitmap{
+		data: make([]byte, (size+7)/8),
+		size: size,
+	}
+}
+
+func (b *bitmap) Set(index int) {
+	b.data[index/8] |= 1 << uint(index%8)
+}
+
+func (b *bitmap) Unset(index int) {
+	b.data[index/8] &= ^(1 << uint(index%8))
+}
+
+func (b *bitmap) IsSet(index int) bool {
+	return b.data[index/8]&(1<<uint(index%8)) != 0
+}
